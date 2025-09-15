f60a4098962720d3861cf2e9b17188050411636d
jam3sl1ng <jamesling596@gmail.com>
2025-09-15 11:15:45 +0100

    V0.2

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,10 @@ use std::io;
 use std::process;
 
 fn main() {
-    print_menu();
+    init_page();
 }
 
-fn print_menu() {
-    let mut page_selection: String = String::new();
-
+fn print_ascii() {
     println!(r#"
     $$$$$$$\                        $$\           $$\      $$\                     $$\           
     $$  __$$\                       $$ |          $$$\    $$$ |                    \__|          
@@ -17,18 +15,52 @@ fn print_menu() {
     $$ |  $$ |$$ |  $$ | \____$$\   $$ |$$\       $$ |\$  /$$ |$$ |  $$ | \____$$\ $$ |$$ |      
     $$ |  $$ |\$$$$$$  |$$$$$$$  |  \$$$$  |      $$ | \_/ $$ |\$$$$$$  |$$$$$$$  |$$ |\$$$$$$$\
     \__|  \__| \______/ \_______/    \____/       \__|     \__| \______/ \_______/ \__| \_______|"#);
+}
+
+fn clear_screen() {
+    print!("\x1B[2J\x1B[1;1H");
+}
+
+fn init_page() {
+    clear_screen();
+
+    let mut page_selection: String = String::new();
+
+    print_ascii();
 
     println!(r#"
-    [1] Play
-    [2] Leader Bosard
-    [3] Quit"#);
+    [1] Login
+    [2] Quit"#);
     io::stdin()
         .read_line(&mut page_selection)
         .expect("Please select a valid option (1 - 3).");
 
-    println!("You selected page: {}", page_selection);
+    match page_selection.trim() {
+        "1" => print_menu(),
+        "2" => process::exit(0x100),
+        _ => println!("Invalid selection. Please try again."),
+    }
+}
 
-    if page_selection == "3" {
-        process::exit(0x100);
+fn print_menu() {
+    clear_screen();
+
+    let mut page_selection: String = String::new();
+
+    print_ascii();
+
+    println!(r#"
+    [1] Play
+    [2] Leader Bosard
+    [3] Logout"#);
+    io::stdin()
+        .read_line(&mut page_selection)
+        .expect("Please select a valid option (1 - 3).");
+
+    match page_selection.trim() {
+        "1" => print_menu(),
+        "2" => print_menu(),
+        "3" => init_page(),
+        _ => println!("Invalid selection. Please try again."),
     }
 }
