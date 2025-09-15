701bedb3672b486b06f73265deff2fdca2e2618d
jam3sl1ng <jamesling596@gmail.com>
2025-09-15 11:01:19 +0100

    V0.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io;
+use std::process;
+
 fn main() {
-    println!("Hello, world!")
+    print_menu();
+}
+
+fn print_menu() {
+    let mut page_selection: String = String::new();
+
+    println!(r#"
+    $$$$$$$\                        $$\           $$\      $$\                     $$\           
+    $$  __$$\                       $$ |          $$$\    $$$ |                    \__|          
+    $$ |  $$ |$$\   $$\  $$$$$$$\ $$$$$$\         $$$$\  $$$$ |$$\   $$\  $$$$$$$\ $$\  $$$$$$$\ 
+    $$$$$$$  |$$ |  $$ |$$  _____|\_$$  _|        $$\$$\$$ $$ |$$ |  $$ |$$  _____|$$ |$$  _____|
+    $$  __$$< $$ |  $$ |\$$$$$$\    $$ |          $$ \$$$  $$ |$$ |  $$ |\$$$$$$\  $$ |$$ /      
+    $$ |  $$ |$$ |  $$ | \____$$\   $$ |$$\       $$ |\$  /$$ |$$ |  $$ | \____$$\ $$ |$$ |      
+    $$ |  $$ |\$$$$$$  |$$$$$$$  |  \$$$$  |      $$ | \_/ $$ |\$$$$$$  |$$$$$$$  |$$ |\$$$$$$$\
+    \__|  \__| \______/ \_______/    \____/       \__|     \__| \______/ \_______/ \__| \_______|"#);
+
+    println!(r#"
+    [1] Play
+    [2] Leader Bosard
+    [3] Quit"#);
+    io::stdin()
+        .read_line(&mut page_selection)
+        .expect("Please select a valid option (1 - 3).");
+
+    println!("You selected page: {}", page_selection);
+
+    if page_selection == "3" {
+        process::exit(0x100);
+    }
 }
